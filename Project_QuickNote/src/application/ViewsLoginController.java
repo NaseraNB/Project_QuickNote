33b0758934afb1f6a24905f52478b32911f84935
nasera <nasera@java.myguest.virtualbox.org>
2023-05-26 10:36:47 +0200

    6. Hem afegit al controlador del login la ruta de les imatges, a banda que hem afegit nous id al login

--- Project_QuickNote/src/application/ViewsLoginController.java
+++ Project_QuickNote/src/application/ViewsLoginController.java
@@ -2,15 +2,23 @@ package application;
 
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 
 
-public class ViewsLoginController {
+public class ViewsLoginController implements Initializable{
 
 	// Variables que vamos a utilizar que an sido inicializadas en Login.FXML.
 	@FXML
@@ -29,10 +37,34 @@ public class ViewsLoginController {
 	private Button registerButton;
 	
 	@FXML
+	private Label loginMessage;
 	
+	@FXML 
+	private ImageView imageLogo;
+	
+	@FXML 
+	private ImageView ImageIcon;
+	
+	public void loginMessageOnAction(ActionEvent event) {
+		loginMessage.setText("You try to login");
+	}
+	
+	@FXML
 	public void cancelButtonOnAction(ActionEvent event) {
 		Stage stage = (Stage) cancelButton.getScene().getWindow();
 		stage.close();
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle arg1) {
+		File logoFile = new File("/home/nasera/git/Repository_YN/Project_QuickNote/Image/Captura desde 2023-05-26 06-21-34.png");
+		Image logoImage = new Image(logoFile.toURI().toString());
+		imageLogo.setImage(logoImage);
+		
+		File iconFile = new File("/home/nasera/git/Repository_YN/Project_QuickNote/Image/Captura desde 2023-05-26 06-35-02.png");
+		Image iconImage = new Image(iconFile.toURI().toString());
+		ImageIcon.setImage(iconImage);
+		
 	}
 	
 }
