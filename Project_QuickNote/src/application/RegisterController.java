e1a13195e07072901d60b6a17c412c03f017cfbf
nasera <nasera@java.myguest.virtualbox.org>
2023-05-27 18:48:19 +0200

    25. Classe controlador de registre buit.

--- Project_QuickNote/src/application/RegisterController.java
+++ Project_QuickNote/src/application/RegisterController.java
@@ -1,5 +1,15 @@
 package application;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+
+import java.io.File;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.Statement;
+
 public class RegisterController {
 
 }
