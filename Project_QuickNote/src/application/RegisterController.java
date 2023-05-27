b27b96afd10f910a2e0d82fc3dd1ee3582da0ef6
nasera <nasera@java.myguest.virtualbox.org>
2023-05-27 19:52:57 +0200

    33. Hem afegit el botó de tancar el registre

--- Project_QuickNote/src/application/RegisterController.java
+++ Project_QuickNote/src/application/RegisterController.java
@@ -1,5 +1,6 @@
 package application;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -8,6 +9,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.PasswordField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 import java.io.File;
 import java.net.URL;
@@ -20,15 +22,25 @@ public class RegisterController implements Initializable{
 	@FXML
 	private ImageView iconSignin;
 	
+	@FXML
+	private Button closeButtonS; 
+	
 	
 	@Override
 	public void initialize(URL url, ResourceBundle arg1) {
-		
 		// Carga la image del icon atravez de la ruta donde se encuantra.
+		
 		File iconSignInFile = new File("/home/nasera/git/Repository_YN/Project_QuickNote/Image/icon4.png");
 		Image iconSignInImage = new Image(iconSignInFile.toURI().toString());
-		iconSignin.setImage(iconSignInImage);
-				
+		iconSignin.setImage(iconSignInImage);		
+	}
+	
+	@FXML
+	public void cancelButtonOnAction(ActionEvent event) {
+		
+		// Cerramos el registro, si el usuario pulsa el boton de close le llevará  a la vista del login.
+		Stage stage = (Stage) closeButtonS.getScene().getWindow();
+		stage.close();
 	}
 
 }
