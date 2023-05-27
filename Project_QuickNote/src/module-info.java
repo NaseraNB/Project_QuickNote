2f9bd6d80913fe88b5700d940d0182f0ce1187a1
nasera <nasera@java.myguest.virtualbox.org>
2023-05-27 15:16:51 +0200

    18. Update

--- Project_QuickNote/src/module-info.java
+++ Project_QuickNote/src/module-info.java
@@ -2,6 +2,8 @@ module Project_QuickNote {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires java.desktop;
+	requires java.sql;
+	requires mysql.connector.java;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
